e78f4dcc7ad0576eee9b6fa33e6bf9987e3aaf17
DiscreteTom <discrete_tom@outlook.com>
2022-12-27 20:31:48 +0800

    feat: support nested XML tags

--- src/patcher.ts
+++ src/patcher.ts
@@ -1,53 +1,77 @@
 import { Lexer } from "marked";
-import { XMLParser } from "fast-xml-parser";
+import { XMLParser, XMLBuilder } from "fast-xml-parser";
 import { readFileSync } from "fs";
 import { config } from "./arg";
+import * as jp from "jsonpath";
 
 export function patch(content: string) {
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: "",
+  });
+  const builder = new XMLBuilder({ processEntities: false });
+
   return new Lexer()
     .lex(content)
     .map((t) => {
-      if (t.type != "html") return t.raw;
+      if (t.type != "html") return t.raw; // normal markdown, not XML tag
 
-      const obj = new XMLParser({
-        ignoreAttributes: false,
-        attributeNamePrefix: "",
-      }).parse(t.raw);
+      const obj = [{ top: parser.parse(t.raw) }]; // wrap the XML content
 
-      if (!(config.tag in obj)) return t.raw;
+      jp.query(obj, `$..*[?(@.${config.tag})]`) // find all nodes with the tag
+        .map((node) => {
+          let result = "";
 
-      const attr = obj[config.tag];
+          const elements: {
+            path: string;
+            from?: string;
+            to?: string;
+            type?: string;
+            lang?: string;
+          }[] =
+            node[config.tag] instanceof Array
+              ? node[config.tag]
+              : [node[config.tag]];
 
-      const options = {
-        from: 0,
-        to: undefined,
-        type: "code",
-        lang: "",
-      };
+          elements.map((el) => {
+            const options = {
+              from: 0,
+              to: undefined,
+              type: "code",
+              lang: "",
+            };
 
-      if (!attr.path) throw new Error(`Missing path.`);
-      options.lang = attr.lang || attr.path.split(".").at(-1);
-      if (typeof attr.from == "string") {
-        options.from = Number(attr.from) - 1;
-      }
-      if (typeof attr.to == "string") {
-        options.to = Number(attr.to);
-      }
-      if (attr.type) options.type = attr.type;
+            if (!el.path) throw new Error(`Missing path.`);
+            options.lang = el.lang || el.path.split(".").at(-1);
+            if (typeof el.from == "string") {
+              options.from = Number(el.from) - 1;
+            }
+            if (typeof el.to == "string") {
+              options.to = Number(el.to);
+            }
+            if (el.type) options.type = el.type;
 
-      const file_content = readFileSync(attr.path, "utf-8")
-        .split("")
-        .filter((c) => c != "\r")
-        .join("")
-        .split("\n")
-        .slice(options.from, options.to)
-        .join("\n");
+            const file_content = readFileSync(el.path, "utf-8")
+              .split("")
+              .filter((c) => c != "\r")
+              .join("")
+              .split("\n")
+              .slice(options.from, options.to)
+              .join("\n");
 
-      if (options.type == "code") {
-        return `\`\`\`${options.lang}\n${file_content}\n\`\`\`\n\n`;
-      } else {
-        throw new Error(`Invalid type: ${options.type}`);
-      }
+            if (options.type == "code") {
+              result += `\n\n\`\`\`${options.lang}\n${file_content}\n\`\`\`\n\n`;
+            } else {
+              throw new Error(`Invalid type: ${options.type}`);
+            }
+          });
+
+          delete node[config.tag];
+          node["#text"] = result; // apply text content as the XML content
+          return node;
+        });
+      const [{ top: content }] = obj; // unwrap the content
+      return builder.build(content) + "\n\n"; // convert the XML object back to text
     })
     .join("");
 }
